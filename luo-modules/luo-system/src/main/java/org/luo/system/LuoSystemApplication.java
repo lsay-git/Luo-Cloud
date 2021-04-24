bc94ba34d9dcb0069d5152a36eb13e6606e26101
lsay <lsay0928@outlook.com>
2021-04-24 09:26:25 +0800

    first commit

---
+++ luo-modules/luo-system/src/main/java/org/luo/system/LuoSystemApplication.java
@@ -0,0 +1,13 @@
+package org.luo.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuoSystemApplication.class, args);
+    }
+
+}
---
+++ luo-modules/luo-system/src/test/java/org/luo/system/LuoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.luo.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
