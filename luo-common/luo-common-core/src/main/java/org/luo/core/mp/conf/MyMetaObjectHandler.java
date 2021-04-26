814179ef00355b02cdb4eb9f7023848093003ef5
kqq22 <2765618996@qq.com>
2021-04-26 21:57:13 +0800

    update

--- luo-common/luo-common-core/src/main/java/org/luo/core/mp/conf/MyMetaObjectHandler.java
+++ luo-common/luo-common-core/src/main/java/org/luo/core/mp/conf/MyMetaObjectHandler.java
@@ -34,9 +34,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         this.strictInsertFill(metaObject, CREATE_TIME, LocalDateTime.class, LocalDateTime.now());
         this.strictInsertFill(metaObject, CREATE_USER, Long.class, 0L);
         this.strictInsertFill(metaObject, UPDATE_USER, Long.class, 0L);
-//        this.setFieldValByName(CREATE_USER, 0,metaObject);
         this.strictInsertFill(metaObject, UPDATE_TIME, LocalDateTime.class, LocalDateTime.now());
-//        this.setFieldValByName(UPDATE_USER, 0,metaObject);
     }
 
     @Override
